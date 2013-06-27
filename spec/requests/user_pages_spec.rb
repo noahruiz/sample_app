d4f6e8823d71ffec6adf42bad97178018556aba9
Noah Ruiz <noahruiz@funcsub.com>
2013-06-27 11:31:10 -0700

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -7,8 +7,8 @@ describe "User pages" do
 	describe "signup page" do
 		before { visit signup_path }
 
-		it { should have_selector('h1',    text: 'Sign up')}
-		it { should have_selector('title', text: full_title('Sign up')) }
+		it { should have_selector('h1',    text: 'Sign up') }
+		it { should have_selector('title', text: 'Sign up') }
 	end
 
 	describe "profile page" do
@@ -20,7 +20,7 @@ describe "User pages" do
 		it { should have_selector('title', text: user.name) }
 	end
 
-	describe "signup" do
+	describe "signup" do 
 
 		before { visit signup_path }
 
@@ -36,6 +36,7 @@ describe "User pages" do
 
 				it { should have_selector('title', text: 'Sign up') }
 				it { should have_content('error') }
+				it { should_not have_content('Password digest') }
 			end
 		end
 
@@ -54,10 +55,12 @@ describe "User pages" do
 
 			describe "after saving a user" do
 				before { click_button submit }
+
 				let(:user) { User.find_by_email("user@example.com") }
 
 				it { should have_selector('title', text: user.name) }
 				it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+				it { should have_link('Sign out') }
 			end
 		end
 	end
